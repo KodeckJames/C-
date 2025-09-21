21d36278a2fd9b5a81613f3c5bde7f65bcbea5f6
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-09-21 20:02:30 +0300

    First C++ HelloWorld program

---
+++ 1_helloWorld.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+    // This is a comment :)
+    /*
+    This 
+    is
+    a
+    multiline
+    comment
+    */
+    std::cout << "Hello, World!" << '\n';
+    std::cout << "This is my first C++ program" << std::endl;
+    return 0;
+}
